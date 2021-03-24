0dad49c3cfcbf92eee52ad8bf2c8474dc6e23919
Teyeb Amani <amani.teyeb2@gmail.com>
2021-03-24 12:20:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import imageInSrc  from './Images/bmw.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style= {{border:'solid 1px black', maxWidth:'100vw'}}>
+
+      <h1 className ="title red">BMW cars</h1>
+
+     <br/>
+
+     <img src={imageInSrc}
+     width = '800px'
+     length= '600px'
+     alt="img"/>
+
+     <br/>
+
+     <img src="./images/bmw.jpg" 
+      width = '800px'
+      length= '600px'
+     alt="img"/>
+
+     </div>
+     <iframe 
+     title="video-main"
+     width="320" 
+     height="240" 
+     src="https://www.youtube.com/embed/kPAjkaTIelY" 
+     frameborder="0" 
+     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+    ></iframe>
     </div>
   );
 }
